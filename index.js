52bcc03856dd89998fcdef53ce9045e298929625
Jason Dudley <jason@jddudley.com>
2017-01-26 17:36:59 -0700

    New Background

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    //smooth scroll
+// $(document).scroll('smooth')
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
